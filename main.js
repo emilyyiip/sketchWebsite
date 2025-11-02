d9f33d592950cf55cc7024bab24c49144def6683
emilyyiip <144075069+emilyyiip@users.noreply.github.com>
2025-11-02 14:44:56 -0800

    image positioning

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 console.log("working")
-const vid = document.getElementById("video");
-vid.playbackRate = 4.0;
+const vid = document.getElementById("visualVid");
+const vis = document.getElementById("visual")
+const img = document.getElementById("visualImg")
 
-vid.addEventListener("mouseenter", () => {
+//vid.playbackRate = 4.0;
+
+img.addEventListener("mouseenter", () => {
     vid.currentTime = 0;
     vid.play();
 });
