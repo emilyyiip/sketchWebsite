ef93361d5bf066fe3e9cdc604f5715db972de35d
emilyyiip <144075069+emilyyiip@users.noreply.github.com>
2025-11-21 18:31:26 -0800

    art blog

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-console.log("working")
 const vid = document.getElementById("visualVid");
 const vis = document.getElementById("visual")
 const img = document.getElementById("visualImg")
 
-//vid.playbackRate = 4.0;
+vid.playbackRate = 4.0;
 
-img.addEventListener("mouseenter", () => {
+vid.addEventListener("mouseenter", () => {
     vid.currentTime = 0;
     vid.play();
 });
